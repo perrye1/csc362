695cb417ca57d51e7514e4110c7c79f37114a598
Ethan Perry <perrye1@nku.edu>
2015-06-05 18:27:52 -0700

    Uploading Assignment 1

---
+++ PR_Assignment_1/PR_Assignment_1/PR_Assignment_1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>                      // for file functions
+#include <ctype.h>
+
+int main()
+{
+	char c;
+	char last_alpha;
+	int cap_flag = 1;
+	FILE *fp1, *fp2;		            // fp1 points to the input file, fp2 points to the output file
+	fp1 = fopen("input2.txt", "r");	    // open input file, read-only
+	fp2 = fopen("output1.txt", "w");	// open output file, write-only
+
+	while ((c = getc(fp1)) != -1)            // get next char from input file, store in x, until we reach EOF
+	{
+		if (isalpha(c))
+		{
+			if (isupper(c))
+			{
+				if (cap_flag == 1)
+				{
+					putc(c, fp2);                   // output the character to the output file
+					cap_flag = 0;
+				}
+				else if (islower(last_alpha))
+				{
+					putc(tolower(c), fp2);
+				}
+			}
+			else
+			{
+				if (isupper(last_alpha))
+				{
+					putc(c, fp2);
+				}
+			}
+			last_alpha = c;
+		}
+		else
+		{
+			putc(c, fp2);
+			if (c == '.')
+			{
+				cap_flag = 1;
+			}
+		}
+
+	}
+}
+
