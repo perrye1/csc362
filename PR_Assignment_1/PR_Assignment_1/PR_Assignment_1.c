b3475ef753e8b7bedf21b9427f584e850acf5f27
Ethan Perry <perrye1@nku.edu>
2015-06-07 13:45:16 -0700

    added counter variables and incrementing functionality

--- PR_Assignment_1/PR_Assignment_1/PR_Assignment_1.c
+++ PR_Assignment_1/PR_Assignment_1/PR_Assignment_1.c
@@ -6,12 +6,15 @@ int main()
 	char c;
 	char last_alpha;
 	int cap_flag = 1;
+	int in_count;
+	int out_count;
 	FILE *fp1, *fp2;		            // fp1 points to the input file, fp2 points to the output file
 	fp1 = fopen("input2.txt", "r");	    // open input file, read-only
 	fp2 = fopen("output1.txt", "w");	// open output file, write-only
 
 	while ((c = getc(fp1)) != -1)            // get next char from input file, store in x, until we reach EOF
 	{
+		in_count++;
 		if (isalpha(c))
 		{
 			if (isupper(c))
@@ -19,11 +22,13 @@ int main()
 				if (cap_flag == 1)
 				{
 					putc(c, fp2);                   // output the character to the output file
+					out_count++;
 					cap_flag = 0;
 				}
 				else if (islower(last_alpha))
 				{
 					putc(tolower(c), fp2);
+					out_count++;
 				}
 			}
 			else
@@ -31,6 +36,7 @@ int main()
 				if (isupper(last_alpha))
 				{
 					putc(c, fp2);
+					out_count++;
 				}
 			}
 			last_alpha = c;
@@ -38,6 +44,7 @@ int main()
 		else
 		{
 			putc(c, fp2);
+			out_count++;
 			if (c == '.')
 			{
 				cap_flag = 1;
