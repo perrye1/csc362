aa6ab1e1ea6735a0bc29aa334d2a34b61e0509d4
perrye1 <perrye1@nku.edu>
2015-06-24 17:13:15 -0400

    some stuff

--- PR_Assignment_2/PR_Assignment_2/io_summary.c
+++ PR_Assignment_2/PR_Assignment_2/io_summary.c
@@ -1,12 +1,35 @@
-#include<c:\users\perrye1.perry\documents\githubvisualstudio\csc362\pr_assignment_2\PR_Assignment_2\prototypes.h>
+#include<prototypes.h>
 
 int inputLine(FILE *fp_in, char *first_name, char *last_name, int *h1, int *h2, int *h3, int *h4, int *p1, int *p2, int *p3, int *p4, int *p5, int *p6, int *m1, int *m2, int *m3, int *ec)
 {
-	//TODO: input data
+	int num;
+	num = fscanf(fp_in,"%s %s %d %d %d %d %d %d %d %d %d %d %d %d %d %d", first_name, last_name, h1,h2,h3,h4,p1,p2,p3,p4,p5,p6,m1,m2,m3,ec);
+	return num;
 }
-void outputLine(FILE *fp_out, char first_name, char last_name, double average, char leter_grade)
+void outputLine(FILE *fp_out, char *first_name, char *last_name, double average)
 {
-	fprintf(fp_out, "Student: %s %s has average of %4.2d%% and grade of %s", first_name, last_name, average, leter_grade);
+	char letter_grade[2];
+	if (average >= 90) 
+	{
+		strcpy(letter_grade,"A");
+	}
+	else if(average <= 89 && average >= 80)
+	{
+		strcpy(letter_grade, "B");
+	}
+	else if (average <= 79 && average >= 70)
+	{
+		strcpy(letter_grade, "C");
+	}
+	else if (average <= 69 && average >= 60)
+	{
+		strcpy(letter_grade, "D");
+	}
+	else if (average <= 59)
+	{
+		strcpy(letter_grade, "F");
+	}
+	fprintf(fp_out,"Student: %s %s has average of %4.2f%% and grade of %s\n", first_name, last_name, average, letter_grade);
 }
 void summarize(FILE *fp_out, int total_students, double total_average, int total_passing, int total_failing)
 {
--- PR_Assignment_2/PR_Assignment_2/main.c
+++ PR_Assignment_2/PR_Assignment_2/main.c
@@ -1,13 +1,15 @@
-#include <c:\users\perrye1.perry\documents\githubvisualstudio\csc362\pr_assignment_2\PR_Assignment_2\prototypes.h> 
+#include <prototypes.h> 
 
 void main(){
 	char in_file[] = "grades1.txt";			// the input file
 	char out_file[] = "output.txt";
-	FILE *fp_in, *fp_out;						// fp1 points to the input file, fp2 points to the output file
+	FILE *fp_in;
+	FILE *fp_out;						// fp1 points to the input file, fp2 points to the output file
 	int file_length;
 	int i, h1, h2, h3, h4, p1, p2, p3, p4, p5, p6, m1, m2, m3, ec, total_students, total_passing, total_failing, num_params;
 	double average, total_average;
-	char first_name[20], last_name[20], letter_grade;
+	char first_name[20];
+	char last_name[20];
 
 	fp_in = fopen(in_file, "r");
 	fp_out = fopen(out_file, "w");
@@ -16,15 +18,15 @@ void main(){
 	for (i=0;i < file_length;i++) 
 	{
 		num_params = inputLine(fp_in, first_name, last_name, &h1, &h2, &h3, &h4, &p1, &p2, &p3, &p4, &p5, &p6, &m1, &m2, &m3, &ec);
-		if (num_params = 16)
+		if (num_params == 16)
 		{
-			computeGrade(h1, h2, h3, h4, p1, p2, p3, p4, p5, p6, m1, m2, m3, ec, &average, &letter_grade);
-			outputLine(fp_out, first_name, last_name, average, letter_grade);
-			update(&total_students, &total_average, &total_passing, &total_failing);
+			computeGrade(h1, h2, h3, h4, p1, p2, p3, p4, p5, p6, m1, m2, m3, ec, &average);
+			outputLine(fp_out, first_name, last_name, average);
+			update(average, &total_students, &total_average, &total_passing, &total_failing);
 		}
 		else
 		{
-			fprintf(fp_out, "Error attempting to read line %d, skipping record", i);
+			fprintf(fp_out, "Error attempting to read line %d, skipping record\n", i+1);
 		}
 		
 	}
