00419c6af12a5b7f6fde94ef27d5c979e9a2cef5
Ethan Perry <perrye1@nku.edu>
2015-07-07 21:39:23 -0400

    added some comments

--- PR_Assignment_3/PR_Assignment_3/main.c
+++ PR_Assignment_3/PR_Assignment_3/main.c
@@ -6,39 +6,41 @@
 
 #include<stdio.h>
 #include<time.h>
-void printBoard(FILE *fp_out, char *a);		// print the string character by character using pointer arithmetic
-int move(char *p, char *board, int turn);
+
+void printBoard(FILE *fp_out, char *a, char *p1, char *p2);		// print the board after every turn, showing the changes
+int move(char *p, char *p_opp, char *board);					// moves the player through the board
+int find_haven(char *p, char *board);							// finds the next haven on the board, either forward or backward
 
 void main() 
 {
-	srand(time(NULL));
-	char out_file[] = "output.txt";			// the output file
-	FILE *fp_out;							// output file pointer
-	fp_out = fopen(out_file, "a");			// open the output file to write
+	srand(time(NULL));											// seed for the random number generator
+	char out_file[] = "output.txt";								// the output file
+	FILE *fp_out;												// output file pointer
+	fp_out = fopen(out_file, "a");								// open the output file to write
 	char board[100] = "  mHk nH l B He Flq p H  hByHlho H B  jr HFB ir j H  F ku gd  H pjB mH x  BF i H  m oB HlHFBhoH BB ";
-	char *p1 = board;
-	char *p2 = board;
+	char *p1 = board;											// pointer for player 1
+	char *p2 = board;											// pointer for player 2
 	while (1)
 	{
-		printf("player 1: ");
-		p1=move(p1, p2, board);
-		if (p1 >= board + 100)
+		printf("player 1: ");										
+		p1=move(p1, p2, board);									// call the move function on player 1, also passing player 2 so we can check for collisions
+		if (p1 >= board + 100)									// if the player pointer is pointing at the end of the board, that player has won
 		{
 			printf("Player 1 has won the game\n");
 			break;
 		}
 		printf("player 2: ");
-		p2=move(p2, p1, board);
-		if (p2 >= board + 100)
+		p2=move(p2, p1, board);									// call the move function on player 1, also passing player 2 so we can check for collisions
+		if (p2 >= board + 100)									// if the player pointer is pointing at the end of the board, that player has won
 		{
 			printf("Player 2 has won the game\n");
 			break;
 		}
-		printBoard(fp_out, board, p1, p2);
+		printBoard(fp_out, board, p1, p2);						// calls the printBoard function after every turn
 	}	
 }
 
-int move(char *p, char *p_opp, char *board)
+int move(char *p, char *p_opp, char *board)						// moves the player through the board
 {
 	int dice_roll = rand() % 6 + 1;
 	p=p+dice_roll;
@@ -78,28 +80,28 @@ int move(char *p, char *p_opp, char *board)
 	return p;
 }
 
-void printBoard(FILE *fp_out, char *a, char *p1, char *p2)	// print the string pointed to by a (which is the board)
+void printBoard(FILE *fp_out, char *a, char *p1, char *p2)	// print the board after every turn, showing the changes
 {
-	while (*a != '\0')						// while we haven't hit the end of the string
+	while (*a != '\0')										// while we haven't hit the end of the string
 	{
 		if (a == p1)
 		{
-			putc('1', fp_out);				// if the current character is where the player 1 is, print a 1
+			putc('1', fp_out);								// if the current character is where the player 1 is, print a 1
 		}
 		else if (a == p2)
 		{
-			putc('2', fp_out);				// if the current character is where the player 2 is, print a 2
+			putc('2', fp_out);								// if the current character is where the player 2 is, print a 2
 		}
 		else 
 		{
-			putc(*a, fp_out);				// print the current character
+			putc(*a, fp_out);								// print the current character
 		}
-		a++;								// move on to the next character of a
+		a++;												// move on to the next character of a
 	}
-	putc('\n', fp_out);						// go to the next line
+	putc('\n', fp_out);										// go to the next line
 }
 
-int find_haven(char *p, char *board)
+int find_haven(char *p, char *board)						// finds the next haven on the board, either forward or backward
 {
 	int count = 0;
 	if (*p == 'B')
